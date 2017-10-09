ba507b006ddbf3482e09874dc506addb5b358bc4
Peter Anderson <peter@panda.id.au>
2017-10-09 16:48:50 +1100

    Removed debug code.

--- js/ff.js
+++ js/ff.js
@@ -3,7 +3,6 @@
 Backdrop.behaviors.freezeframe = {
   attach: function(context, settings) {
 
-    console.log(settings.freezeframe);
     $(settings.freezeframe.gifs).freezeframe({
       'animation_play_duration': Number(settings.freezeframe.duration),
       'non_touch_device_trigger_event': settings.freezeframe.trigger
