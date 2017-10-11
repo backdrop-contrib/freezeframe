567f41499348e4dd8ce64c16dd6f6052c57cc0c7
Peter Anderson <peter@panda.id.au>
2017-10-12 08:00:49 +1100

    Fixed 'no images found for selector...' JS error.

--- js/ff.js
+++ js/ff.js
@@ -3,10 +3,13 @@
 Backdrop.behaviors.freezeframe = {
   attach: function(context, settings) {
 
-    $(settings.freezeframe.gifs).freezeframe({
-      'animation_play_duration': Number(settings.freezeframe.duration),
-      'non_touch_device_trigger_event': settings.freezeframe.trigger
-    });
+    var gifs = $(settings.freezeframe.gifs);
+    if (gifs.length) {
+      gifs.freezeframe({
+        'animation_play_duration': Number(settings.freezeframe.duration),
+        'non_touch_device_trigger_event': settings.freezeframe.trigger
+      });
+    }
 
   }
 };
