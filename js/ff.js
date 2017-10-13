2f2105d311e41a3d5b06479c1b4c745c8eec745a
Peter Anderson <peter@panda.id.au>
2017-10-13 14:51:45 +1100

    Added overlay.

--- js/ff.js
+++ js/ff.js
@@ -3,13 +3,51 @@
 Backdrop.behaviors.freezeframe = {
   attach: function(context, settings) {
 
+    /*** Freeze GIFs ***/
     var gifs = $(settings.freezeframe.gifs);
+
+    // Make sure there are GIFs to freeze.
     if (gifs.length) {
+      // Freeze them, with the specified settings.
       gifs.freezeframe({
         'animation_play_duration': Number(settings.freezeframe.duration),
         'non_touch_device_trigger_event': settings.freezeframe.trigger
       });
     }
+
+
+    /*** Overlay ***/
+    var timer;
+    var touch = ('ontouchstart' in window || 'onmsgesturechange' in window);
+
+    // Toggle the overlay.
+    var toggleOverlay = function() {
+      $('.ff-container').children('.overlay').toggle();
+    }
+
+    // Add the overlay.
+    $('.ff-container').prepend('<div class="overlay"><img src="/core/misc/arrow-asc.png"></div>').each(function() {
+      // Toggle the overlay on click/hover (depending on the settings).
+      if (settings.freezeframe.trigger == 'click' || touch) {
+        $(this).click(function() {
+          toggleOverlay();
+
+          // Toggle again after the specified duration (if any).
+          if (settings.freezeframe.duration != "Infinity") {
+            if ($(this).children('canvas').hasClass('ff-canvas-active')) {
+              timer = setTimeout(toggleOverlay, Number(settings.freezeframe.duration));
+            }
+            else {
+              // Cancel the timer if clicked again before duration expires.
+              clearTimeout(timer);
+            }
+          }
+        });
+      }
+      else {
+        $(this).hover(toggleOverlay);
+      }
+    });
 
   }
 };
